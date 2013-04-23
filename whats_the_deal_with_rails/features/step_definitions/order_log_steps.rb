3c51d086664eaab4f3298f6d2617131536f35451
Ravi Gadad <ravi@smallsleepingtree.com>
2013-04-23 05:20:39 -0700

    Add (failing) step def for attaching file to form
    This next step will necessitate a form.  I made an assumption as to
    the generated form element name, based on the fact that I intend this
    to be a form for a new "OrderLog" instance.  Note that we've also now
    added the fixture file for the first time.

--- whats_the_deal_with_rails/features/step_definitions/order_log_steps.rb
+++ whats_the_deal_with_rails/features/step_definitions/order_log_steps.rb
@@ -1,3 +1,8 @@
 When /^I choose to upload a new file$/ do
   visit new_order_log_url
 end
+
+When /^I upload a valid file$/ do
+  attach_file(:order_log_file, path_to_fixture('order_logs/valid.tab'))
+  click_button 'commit-button'
+end
